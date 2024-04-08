44f7978b32dab28c63d4fd53d7f70c8e475301f1
ktr1133 <kotarou1021@gmail.com>
2024-04-08 19:42:22 +0900

    旧search.phpから内容コピペして全部上書き。web.phpは、guideとcreateを追加

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/guide', function () {
+    return view('guide');
+});
+
+Route::get('/create', function () {
+    return view('search');
 });
