2c5eabaa9e0c709cc13b8e05ed2de31f6f5d024d
ktr1133 <kotarou1021@gmail.com>
2024-04-21 01:02:22 +0900

    ｶﾞｲﾄﾞﾍﾟｰｼﾞとｸﾘｴｲﾄﾍﾟｰｼﾞのﾙｰﾃｨﾝｸﾞを設定

--- app/Http/Controllers/TopRankController.php
+++ app/Http/Controllers/TopRankController.php
@@ -11,7 +11,7 @@ class topRankController extends Controller
      * Display a listing of the resource.
      */
     public function show(){
-        
+        return view('index');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [topRankController::class, 'show']);
 
 Route::get('/guide', [GuideController::class, 'show']);
 
-Route::get('/create', function () {
-    return view('create');
-});
+Route::get('/create', [CreateController::class, 'show']);
