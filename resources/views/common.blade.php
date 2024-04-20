474e64988a33c31ecd7b987f4cf71198c030c6ef
ktr1133 <kotarou1021@gmail.com>
2024-04-20 16:24:32 +0900

    headﾀｸﾞ部分を共通部分のbladeﾌｧｲﾙに追加

---
+++ resources/views/common.blade.php
@@ -0,0 +1,41 @@
+<head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-L279PCKJGD"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-L279PCKJGD');
+    </script>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@dehonjo" />
+    <meta name="description" content="なろう作品のハイファンタジー・VRゲームの良作に出会う機会の創出を目的としたサイトです。独自の指標を設定し、ランキングを作成しています。また、あなたがその指標をカスタマイズすることもできます。">
+    <meta name="robots" content="all">
+    <meta property="og:url" content="https://freenovelanalysis.com/" />
+    <meta property="og:title" content="Search Free Novels" />
+    <meta property="og:description" content="なろう作品, 良作, ランキング作成" />
+    <meta property="og:image" content="https://freenovelanalysis.com/img/twitter_card.png" />
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- CSS -->
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <!-- icon -->
+    <link rel="icon" href="novel_icon.ico">
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@700&display=swap" rel="stylesheet">
+    <!-- 数式表示用 -->
+    <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+
+    <title>Search Narou Works</title>
+</head>
