79261ac1106922202175f1680669c624b66d218c
ktr1133 <kotarou1021@gmail.com>
2024-04-24 00:10:49 +0900

    routeﾌｧｲﾙの参照先を記載

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\TopRankController;
+use App\Http\Controllers\GuideController;
+use App\Http\Controllers\CreateController;
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [topRankController::class, 'show']);
+Route::get('/', [TopRankController::class, 'show']);
 
 Route::get('/guide', [GuideController::class, 'show']);
 
